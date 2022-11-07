24af04a7f883f736c9f5732ca28e5d6190e05f9c
mixnuts07 <french0719@gmail.com>
2022-11-08 01:51:30 +0900

    fix

--- pages/Own/index.tsx
+++ pages/Own/index.tsx
@@ -9,21 +9,30 @@ const Own = () => {
   // TODO ReceivedScoreを受け取る
   // TODO IdentityStateを受け取る
   return (
-    <div className={style.own}>
-      <div className="pt-[130px] w-fit">
-        {/* <div className="pt-[100px] pl-[440px]"> */}
-        <div className="flex flex-row pl-[460px]">
-          <h5 className="text-8xl w-fit">45</h5>
-          <div className="flex flex-col text-4xl pl-[760px]">
-            <h5 className="">450</h5>
-            <h5 className="pt-[80px]">10</h5>
+    <div className="h-screen w-screen bg-home bg-cover">
+      <div className="text-white h-screen w-screen flex flex-col justify-center items-center">
+        <div className="flex flex-row ">
+          <div className="flex flex-col text-4xl items-center">
+            <h5 className="">My Score</h5>
+            <h5 className="">45</h5>
+          </div>
+          <div className="flex flex-col text-4xl pl-40">
+            <div className="flex flex-row">
+              <h5 className="">Total Score:</h5>
+              <h5 className="">450</h5>
+            </div>
+            <div className="flex flex-row text-4xl">
+              <h5 className="">Received Score:</h5>
+              <h5 className="">10</h5>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="text-4xl pt-[160px] pl-[600px]">
-        <p>0x93839a0a9a9</p>
-        <p className="pt-[60px]">0x48ks049wntg</p>
-        <p className="pt-[60px]">0x93839a0a9a9</p>
+        <div className="text-4xl flex flex-col items-center">
+          <p className="pt-[140px]">Identity State</p>
+          <p className="pt-[60px]">0x93839a0a9a9</p>
+          <p className="pt-[60px]">0x48ks049wntg</p>
+          <p className="pt-[60px]">0x93839a0a9a9</p>
+        </div>
       </div>
       <LeftSideBar nowPage="own" />
     </div>
